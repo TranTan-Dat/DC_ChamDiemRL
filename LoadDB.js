da1b704685dd93b3e1ee615124d246f546b5a75b
Azy <74130205+IAmAzy233@users.noreply.github.com>
2021-04-02 11:25:01 +0700

    Load DB len form, chia div form thong tin sv

--- LoadDB.js
+++ LoadDB.js
@@ -9,10 +9,18 @@ export default class LoadDB extends Component {
   {
     sinhvien: 
     {
+      TrangThai: "",
+      BacDaoTao:"",
+      ChuyenNganh: "",
       Lop: " ",
-      mssv: " ",
+      MSSV: " ",
       NgaySinh: " ",
-      hoten: " "
+      NoiSinh: "",
+      HoTen: " ", 
+      DiaChi:"",
+      GioiTinh:"",
+      Khoa:""
+      
     }
   }
   constructor(props) 
@@ -24,10 +32,17 @@ export default class LoadDB extends Component {
           this.setState(
             {  
               sinhvien: {
-                Lop: doc.data().Lop,
-                Masosinhvien:  doc.data().mssv,
-                Ngaysinh:  doc.data().NgaySinh,
-                hoten: doc.data().hoten
+                TrangThai: doc.data().TRANGTHAI,
+                BacDaoTao: doc.data().BacDaoTao,
+                GioiTinh: doc.data().GIOITINH,
+                ChuyenNganh: doc.data().CHUYENNGANH,
+                Lop: doc.data().LOP,
+                MSSV: doc.data().MSSV,
+                NgaySinh: doc.data().NGAYSINH,
+                NoiSinh: doc.data().NOISINH,
+                HoTen: doc.data().HOTEN, 
+                DiaChi:doc.data().DIACHI,
+                Khoa:doc.data().KHOA
               }
             }
           )
@@ -40,15 +55,40 @@ export default class LoadDB extends Component {
         }
           render() {
               return (
-                <View style={styles.flexcontainer}>
-                  <View style={styles.Top} >
-                    <Image source={ require('./rsc/images/Chi_Nguyen.png')} style={styles.img} />
-                  </View>
-                  <View style={styles.bottom}>
-                    <Text> Họ tên: {this.state.sinhvien.hoten} </Text>
-                    <Text> Ngày sinh: {this.state.sinhvien.NgaySinh} </Text>
-                    <Text> Mã số sinh vien: {this.state.sinhvien.mssv} </Text>
-                    <Text> Lớp: {this.state.sinhvien.Lop} </Text>
+                <View style={{width:'100%', height:'98%', }}>
+                  <View style={styles.flexcontainer}>
+                    <View style={styles.Top} >
+                      <Image source={ require('./rsc/images/Chi_Nguyen.png')} style={styles.img} />
+                      <Text style={styles.Name}>{this.state.sinhvien.HoTen}</Text>
+                    </View>
+                    <View style={styles.bottom}>
+                      <View style={styles.flexBottomInform}>
+                        <View style={styles.BottomInformLeft}>
+                          <Text style={styles.Title}>MSSV: </Text>
+                          <Text style={styles.Title}>Trạng thái: </Text>
+                          <Text style={styles.Title}>Giới tính: </Text>
+                          <Text style={styles.Title}>Ngày sinh: </Text>
+                          <Text style={styles.Title}>MSSV: </Text>
+                          <Text style={styles.Title}>Lớp: </Text>
+                          <Text style={styles.Title}>Bậc đào tạo: </Text>
+                          <Text style={styles.Title}>Khoa: </Text>
+                          <Text style={styles.Title}>Chuyên ngành: </Text>
+                          <Text style={styles.Title}>Địa chỉ: </Text>
+                          <Text style={styles.Title}>Số điện thoại: </Text>
+                          <Text style={styles.Title}>Nơi sinh: </Text>
+                          <Text style={styles.Title}>Điểm rèn luyện: </Text>
+                        </View>
+                        <View style={styles.BottomInformRight}>
+                          <Text style={styles.Value}>{this.state.sinhvien.TrangThai}</Text>
+                          <Text style={styles.Value}>{this.state.sinhvien.GioiTinh}</Text>
+                          
+                        </View>
+                      </View>
+                      {/* <Text> Họ tên: {this.state.sinhvien.hoten} </Text>
+                      <Text> Ngày sinh: {this.state.sinhvien.NgaySinh} </Text>
+                      <Text> Mã số sinh vien: {this.state.sinhvien.mssv} </Text>
+                      <Text> Lớp: {this.state.sinhvien.Lop} </Text> */}
+                    </View>
                   </View>
                 </View>
               );
@@ -56,19 +96,41 @@ export default class LoadDB extends Component {
 }
 const styles= StyleSheet.create({
   Top:{
-    flex: 4,
+    height: '40%',
     alignItems:'center',
+    marginTop:15
   },
   img:{
-    width:250,height:250,
+    width:200,height:200,
     borderRadius: 360   
   },
   bottom:{
-    flex: 6, backgroundColor:'blue'
+    height:'60%',
+    backgroundColor:'blue'
   },
   flexcontainer: {
     flex: 1,
-    flexDirection: "column",
+  },
+  flexBottomInform: {
+    flex:1,
+    flexDirection: "row"
+  },
+  BottomInformLeft:{
+    width:'38%', backgroundColor:'pink', marginLeft:'2%'
+  },
+  BottomInformRight:{
+    width:'58%', backgroundColor:'yellow',
+  },
+  Name:{
+    fontSize:30
+  },
+  Title:{
+    fontSize:20,
+    padding:6,
+  },
+  Value:{
+    fontSize:20,
+    padding:6,
   }
 }
 )
