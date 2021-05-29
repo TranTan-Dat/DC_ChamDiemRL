cef4c227eedef8333b026660e106d6c60fc2d56a
Azy <74130205+IAmAzy233@users.noreply.github.com>
2021-05-30 01:53:29 +0700

    trangchonloaiNgDung

--- ChonNgDung.js
+++ ChonNgDung.js
@@ -9,7 +9,8 @@ import {
   Image,
   ImageBackground,
   Dimensions,
-  TouchableOpacity,
+  TouchableHighlight,
+  TouchableOpacity
 } from 'react-native';
 import {StackNavigator} from 'react-navigation';
 import {useNavigation} from '@react-navigation/native';
@@ -19,58 +20,8 @@ import { greaterThan } from 'react-native-reanimated';
 
 const heightS = Dimensions.get('screen').height;
 
-export default class Login extends Component {
+export default class ChonNguoidung extends Component {
   //static navigationOptions = {header: null}
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      Username: '',
-      Password: '',
-      mssv: '',
-      pass: '',
-    };
-  }
-
-  TaoTK = () =>{
-    const {navigate} = this.props.navigation;
-    navigate('ThemSinhVien')
-  }
-
-
-  shoot = async () => {
-    const {Username, mssv, pass, Password} = this.state;
-
-    if (this.state.Username.trim() && this.state.Password.trim()) {
-      const data = await firestore()
-        .collection('SINHVIEN')
-        .doc(Username)
-        .get()
-        .then(snap => {
-          console.log(snap.data());
-          this.setState({
-            mssv: snap.data().MSSV,
-            pass: snap.data().PASS,
-          });
-          console.log(mssv);
-          console.log(pass);
-        });
-
-      if (
-        this.state.Username === this.state.mssv &&
-        this.state.Password === this.state.pass
-      ) {
-        alert('Dang nhap thanh cong');
-        const {navigate} = this.props.navigation;
-        navigate('LoadDB');
-        console.log("thanh cong");
-      } else {
-        alert('Sai ten dang nhap hoac mat khau');
-      }
-    } else {
-      alert('Xin nhap du thong tin');
-    }
-  };
   render() {
     // const {navigate} = this.props.navigation;
     return (
@@ -85,21 +36,17 @@ export default class Login extends Component {
             style={styles.image}
           />
         
-        <Text style={{paddingBottom:15}}>Bạn là Giảng viên hay sinh viên?</Text>
-        <Button
-          title="Giảng viên"
-          color="#841584"
-        />
-        <Text></Text>
-        <Button
-          title="Sinh viên"
-          color="#841584"
-        />
-        </View>
-        <View style={styles.button} >
-          <Text>
-
-          </Text>
+          <Text style={{paddingBottom:15}}>Bạn là Giảng viên hay sinh viên?</Text>
+          <TouchableOpacity
+            style={styles.submit}
+            underlayColor='#fff'>
+              <Text style={[styles.submitText]}>Giảng viên</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.submit}
+            underlayColor='#fff'>
+              <Text style={[styles.submitText]}>Sinh viên</Text>
+          </TouchableOpacity>
         </View>
       </ImageBackground>
       // </SafeAreaView>
@@ -121,5 +68,19 @@ const styles = StyleSheet.create({
   },
   button:{
     color:'#5a9dbe'
+  },
+  submit:{
+    marginTop:10,
+    paddingTop:10,
+    paddingBottom:10,
+    backgroundColor:'#68a0cf',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  submitText:{
+      color:'#fff',
+      textAlign:'center',
+      fontSize:18
   }
 });
